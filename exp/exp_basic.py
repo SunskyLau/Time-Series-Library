56411bddad44a0bfd8d0b00ca6dd67043e3ceb13
liuyangtian <530050237@qq.com>
2025-12-08 20:08:14 +0800

    fix: Clean up model imports by removing unused models from Exp_Basic, which effect bugs.

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -3,8 +3,8 @@ import torch
 from models import Autoformer, Transformer, TimesNet, Nonstationary_Transformer, DLinear, FEDformer, \
     Informer, LightTS, Reformer, ETSformer, Pyraformer, PatchTST, MICN, Crossformer, FiLM, iTransformer, \
     Koopa, TiDE, FreTS, TimeMixer, TSMixer, SegRNN, MambaSimple, TemporalFusionTransformer, SCINet, PAttn, TimeXer, \
-    WPMixer, MultiPatchFormer, KANAD, MSGNet, TimeFilter, Sundial, TimeMoE, Chronos, Moirai, TiRex,\
-    TimesFM, Toto, Chronos2, TFMAE
+    WPMixer, MultiPatchFormer, KANAD, MSGNet, TimeFilter, Sundial, TimeMoE,\
+    TFMAE
 
 
 class Exp_Basic(object):
@@ -45,12 +45,12 @@ class Exp_Basic(object):
             'TimeFilter': TimeFilter,
             'Sundial': Sundial,
             'TimeMoE': TimeMoE,
-            'Chronos': Chronos,
-            'Moirai': Moirai,
-            'TiRex': TiRex,
-            'TimesFM': TimesFM,
-            'Toto': Toto,
-            'Chronos2': Chronos2
+            # 'Chronos': Chronos,
+            # 'Moirai': Moirai,
+            # 'TiRex': TiRex,
+            # 'TimesFM': TimesFM,
+            # 'Toto': Toto,
+            # 'Chronos2': Chronos2,
             'TFMAE': TFMAE
         }
         if args.model == 'Mamba':
